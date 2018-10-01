df969bfa8e06d626f0f9157368191d5d124f4075
Ada 2018 <>
2018-10-01 09:35:40 -0300

    parte ios

--- drinkit/ViewController.swift
+++ drinkit/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var drinkButton: UIButton! {
+        didSet {
+            drinkButton.layer.cornerRadius = 10
+            drinkButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        }
+    }
+    @IBOutlet weak var progressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
