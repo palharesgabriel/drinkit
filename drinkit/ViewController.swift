3f8d3aaa8b6771fc092bce8ff5ddcee3f9459e22
Ada 2018 <>
2018-10-01 10:42:27 -0300

    atualizar agua

--- drinkit/ViewController.swift
+++ drinkit/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var total: Int = 0
 
     @IBOutlet weak var drinkButton: UIButton! {
         didSet {
@@ -16,6 +18,7 @@ class ViewController: UIViewController {
             drinkButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         }
     }
+    
     @IBOutlet weak var progressLabel: UILabel!
     
     override func viewDidLoad() {
@@ -27,11 +30,27 @@ class ViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "drinkSegue" {
+            let vc = segue.destination as! DrinkViewController
+            vc.delegate = self
+        }
     }
 
+}
 
+extension ViewController: DrinkViewControllerDelegate {
+    
+    func updateTotal(drink: Int) {
+        total = total + drink
+        setLabelText()
+    }
+    
+    func setLabelText() {
+        let text = "\(self.total)/2000 ml"
+        progressLabel.text = text
+    }
+    
 }
 
